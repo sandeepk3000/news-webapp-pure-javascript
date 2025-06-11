762b2811730ebe35571674c39903a3676024017c
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-11 11:05:08 +0000

    add server and app file

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
+
+// import routes
+import userRouter from "./routes/user.routes.js";
+import postRouter from "./routes/post.routes.js";
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
+export { app };
