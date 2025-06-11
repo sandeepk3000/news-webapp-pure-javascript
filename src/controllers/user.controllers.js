88e0cdaccbe06830af10cd3a0caab11fa0a4f40a
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-11 10:26:34 +0000

    add register and login controllers

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,97 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import { User } from "../models/user.models.js";
+import uploadOnCloudinary from "../utils/cloudinary.js";
+
+const generateAccessAndRefereshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating referesh and access token",
+    );
+  }
+};
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, fullName, email, password, role } = req.body;
+  if (
+    [username, fullName, email, password, role].some(
+      (field) => field?.trim() === "",
+    )
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  console.log(existedUser);
+  if (existedUser) {
+    throw new ApiError(409, "User with email already exists");
+  }
+  const user = await User.create({
+    username,
+    fullName,
+    email,
+    password,
+    role,
+  });
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken",
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered Successfully"));
+});
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!password && !email) {
+    throw new ApiError(400, "password or email is required");
+  }
+  const user = await User.findOne({
+    $or: [ { email }],
+  });
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+  const isPasswordValid = await user.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+  const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
+    user._id,
+  );
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken",
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged In Successfully",
+      ),
+    );
+});
+
+export { registerUser, loginUser };
