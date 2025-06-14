c6562b519e6ee23d74e756a61e346bc361ff314d
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-14 17:59:52 +0000

    add bookmar post route

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -6,6 +6,8 @@ import {
   getAllPosts,
   updatePost,
   deletePost,
+  bookmarkPost,
+  getTrendingPosts,
 } from "../controllers/post.controllers.js";
 import verifyJWT from "../middlewares/auth.middleware.js";
 import verifyAdmin from "../middlewares/admin.middleware.js";
@@ -15,4 +17,5 @@ router.route("/:postId").get(getPost);
 router.route("/").post(verifyJWT, verifyAdmin, createPost);
 router.route("/:postId").put(verifyJWT, verifyAdmin, updatePost);
 router.route("/:postId").delete(verifyJWT, verifyAdmin, deletePost);
+router.route("/bookmark/:postId").get(verifyJWT, bookmarkPost);
 export default router;
