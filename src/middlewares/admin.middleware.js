4220b7f7538cd30adebd796fefd1ec98a4e0b628
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-19 15:43:56 +0000

    add admin verify middleware

---
+++ src/middlewares/admin.middleware.js
@@ -0,0 +1,11 @@
+import { User } from "../models/user.models.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+const verifyAdmin = asyncHandler(async (req, _, next) => {
+  const user = await User.findById(req.user._id);
+  if (user.role !== "ADMIN") {
+    throw new ApiError(401, "User is not an admin");
+  }
+  next();
+});
+export default verifyAdmin;
