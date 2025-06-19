207e6d815ee2f17924861db6776438150bc21667
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-19 17:01:32 +0000

    add uload-avatar route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/user.controllers";
-
+import {
+  registerUser,
+  loginUser,
+  uploadAvatar,
+} from "../controllers/user.controllers.js";
+import jwtVerify from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 const router = express.Router();
 
 router.route("/").post(registerUser);
 
 router.route("/login").post(loginUser);
+
+router
+  .route("/upload-avatar")
+  .post(jwtVerify, upload.single("avatar"), uploadAvatar);
 
 export default router;
