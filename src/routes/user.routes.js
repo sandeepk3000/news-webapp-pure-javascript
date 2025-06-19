59231986afafbd73fce9e1b0618bc8efae252cfe
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-19 13:30:44 +0000

    add login route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { registerUser } from "../controllers/user.controllers";
+import { registerUser, loginUser } from "../controllers/user.controllers";
 
 const router = express.Router();
 
 router.route("/").post(registerUser);
 
-
+router.route("/login").post(loginUser);
 
 export default router;
