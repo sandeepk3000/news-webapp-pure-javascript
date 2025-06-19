70d673d9a9f4b15c68b08b8c5c6525312c53475c
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-19 13:23:29 +0000

    add user router

--- src/app.js
+++ src/app.js
@@ -10,4 +10,8 @@ app.use(cors(corsOptions));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 export { app };
