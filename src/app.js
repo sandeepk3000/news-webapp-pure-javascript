d943739c0fb1b2cb332051078fe02b29db6cb88a
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-19 16:00:52 +0000

    add post router

--- src/app.js
+++ src/app.js
@@ -12,6 +12,8 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
 import userRouter from "./routes/user.routes.js";
+import postRouter from "./routes/post.routes.js";
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
 export { app };
