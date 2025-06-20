0066d109998ec513b928460fc573e5db003a2c5d
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-20 01:14:35 +0000

    add upload thumbnail route

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { createPost } from "../controllers/post.controllers.js";
+import {
+  createPost,
+  uploadThumbnail,
+} from "../controllers/post.controllers.js";
 import jwtVerify from "../middlewares/auth.middleware.js";
 import verifyAdmin from "../middlewares/admin.middleware.js";
-
+import { upload } from "../middlewares/multer.middleware.js";
 const router = express.Router();
 
 router.route("/").post(jwtVerify, verifyAdmin, createPost);
-
+router
+  .route("/:id/upload-thumbnail")
+  .post(jwtVerify, verifyAdmin, upload.single("thumbnail"), uploadThumbnail);
 export default router;
