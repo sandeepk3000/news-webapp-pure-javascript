661e6c474b425a26a82d314213a25fa58720f585
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-24 15:58:48 +0000

    remove avatar route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,19 +1,9 @@
 import express from "express";
-import {
-  registerUser,
-  loginUser,
-  uploadAvatar,
-} from "../controllers/user.controllers.js";
-import jwtVerify from "../middlewares/auth.middleware.js";
-import { upload } from "../middlewares/multer.middleware.js";
+import { registerUser, loginUser } from "../controllers/user.controllers.js";
 const router = express.Router();
 
 router.route("/").post(registerUser);
 
 router.route("/login").post(loginUser);
-
-router
-  .route("/upload-avatar")
-  .post(jwtVerify, upload.single("avatar"), uploadAvatar);
 
 export default router;
