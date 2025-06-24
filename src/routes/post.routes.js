4eaf378abdf106c89db79c619d067bef603559fc
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-24 15:07:46 +0000

    add post create route

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,19 +1,8 @@
 import express from "express";
-import {
-  publishPost,
-  savePost,
-  uploadThumbnail,
-} from "../controllers/post.controllers.js";
+import { createPost } from "../controllers/post.controllers.js";
 import jwtVerify from "../middlewares/auth.middleware.js";
 import verifyAdmin from "../middlewares/admin.middleware.js";
-import { upload } from "../middlewares/multer.middleware.js";
 const router = express.Router();
 
-router.route("/:id/publish").post(jwtVerify, verifyAdmin, publishPost);
-router.route("/publish").post(jwtVerify, verifyAdmin, publishPost);
-router.route("/:id/save").post(jwtVerify, verifyAdmin, savePost);
-router.route("/save").post(jwtVerify, verifyAdmin, savePost);
-router
-  .route("/:id/upload-thumbnail")
-  .post(jwtVerify, verifyAdmin, upload.single("thumbnail"), uploadThumbnail);
+router.route("/").post(jwtVerify, verifyAdmin, createPost);
 export default router;
