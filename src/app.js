5913714d9e5e787bdffd62169c8a927cd2098546
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-24 15:54:15 +0000

    add media router

--- src/app.js
+++ src/app.js
@@ -13,7 +13,9 @@ app.use(cookieParser());
 
 import userRouter from "./routes/user.routes.js";
 import postRouter from "./routes/post.routes.js";
+import mediaRouter from "./routes/media.routes.js";
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/media", mediaRouter);
 export { app };
