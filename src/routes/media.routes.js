4bce750b46ef906e864772471cf63d55818402f6
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-24 15:53:55 +0000

    add media post media upload route

---
+++ src/routes/media.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { uploadMedia } from "../controllers/media.controllers.js";
+import jwtVerify from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import verifyAdmin from "../middlewares/admin.middleware.js";
+const router = express.Router();
+router
+  .route("/upload-post-media")
+  .post(jwtVerify, verifyAdmin, upload.single("media"), uploadMedia);
+export default router;
