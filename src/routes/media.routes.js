c4b6d1f83787c0132488d6d9bdabac3f383c1a89
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2025-06-24 16:13:29 +0000

    add delete post media route

--- src/routes/media.routes.js
+++ src/routes/media.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { uploadMedia } from "../controllers/media.controllers.js";
+import { deleteMedia, uploadMedia } from "../controllers/media.controllers.js";
 import jwtVerify from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import verifyAdmin from "../middlewares/admin.middleware.js";
@@ -7,4 +7,6 @@ const router = express.Router();
 router
   .route("/upload-post-media")
   .post(jwtVerify, verifyAdmin, upload.single("media"), uploadMedia);
+
+router.route("/delete-post-media").post(jwtVerify, verifyAdmin, deleteMedia);
 export default router;
